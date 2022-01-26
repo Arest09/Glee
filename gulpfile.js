09625595583391d4efc4383601025267e14d80bd
Arest09 <arest09@mail.ru>
2022-01-26 17:27:20 +1000

    footer

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ function browsersync(){
 }
 
 function styles() {
-    return src(['app/scss/style.scss','node_modules/slick-carousel/slick/slick.scss'])
+    return src(['app/scss/style.scss'])
     .pipe(scss({outputStyle:'compressed'}))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({overrideBrowserslist:['last 10 versions'],grid:true}))
